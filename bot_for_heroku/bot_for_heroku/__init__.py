ba46ab6d9083107e0da740c06f5902d5334153f1
wazovski <malika.satimbaeva@iaau.edu.kg>
2021-06-29 17:13:17 +0600

    First

---
+++ bot_for_heroku/bot_for_heroku/__init__.py
@@ -0,0 +1,1 @@
+from bot_for_heroku.settings import *
--- bot_for_heroku/bot_for_heroku/wsgi.py
+++ bot_for_heroku/bot_for_heroku/wsgi.py
@@ -6,11 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bot_for_heroku.settings")
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bot_for_heroku.settings')
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
